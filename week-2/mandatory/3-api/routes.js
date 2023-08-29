3a5a280d3c65ca74df446e580507a8dd6bac3702
Maksim Lukianenko <maxbuzz8694@gmail.com>
2023-08-30 00:07:43 +0100

    chore(package.json): add body-parser dependency to handle JSON parsing in requests feat(routes.js): add GET /customers/:cid endpoint to retrieve a specific customer by id feat(routes.js): add POST /customers endpoint to create a new customer feat(routes.js): add GET /products endpoint to retrieve products with optional name filter feat(routes.js): add POST /products endpoint to create a new product chore(server.js): add body-parser middleware to parse JSON in requests

--- week-2/mandatory/3-api/routes.js
+++ week-2/mandatory/3-api/routes.js
@@ -13,6 +13,40 @@ router.get("/customers", async (req, res) => {
   }
 });
 
+router.get("/customers/:cid", async (req, res) => {
+  const custId = parseInt(req.params.cid);
+  try {
+    const result = await db.query("SELECT * FROM customers WHERE id= $1", [
+      custId,
+    ]);
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.post("/customers", async (req, res) => {
+  const newName = req.body.name;
+  const newAddress = req.body.address;
+  const newCity = req.body.city;
+  const newCountry = req.body.country;
+
+  try {
+    const result = await db.query(
+      "INSERT INTO customers (name, address, city, country) \
+    VALUES ($1, $2, $3, $4) RETURNING id",
+      [newName, newAddress, newCity, newCountry]
+    );
+    const newId = result.rows[0].id;
+    console.log(`New Customer id = ${newId}`);
+    res.status(200).json({ lastId: newId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
 router.get("/suppliers", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM suppliers");
@@ -26,18 +60,41 @@ router.get("/suppliers", async (req, res) => {
 router.get("/products", async (req, res) => {
   const queryPattern = req.query.name;
   try {
-    const result = await db.query(
-      "SELECT \
-       products.id, products.product_name, \
-       product_availability.unit_price, \
-       suppliers.supplier_name \
-       FROM products \
-       JOIN product_availability ON products.id=product_availability.prod_id \
-        JOIN suppliers ON product_availability.supp_id=suppliers.id \
-        WHERE product_name ILIKE $1",
-      [`%${queryPattern}`]
-    );
+    let query = `
+    SELECT
+      products.id, products.product_name,
+      product_availability.unit_price,
+      suppliers.supplier_name
+    FROM products
+    JOIN product_availability ON products.id=product_availability.prod_id
+    JOIN suppliers ON product_availability.supp_id=suppliers.id
+  `;
+
+    if (queryPattern) {
+      query += "WHERE product_name ILIKE $1";
+    }
+
+    const params = queryPattern ? [`%${queryPattern}`] : [];
+
+    const result = await db.query(query, params);
     res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.post("/products", async (req, res) => {
+  const newName = req.body.name;
+  try {
+    const result = await db.query(
+      "INSERT INTO products (product_name) \
+    VALUES ($1) RETURNING id",
+      [newName]
+    );
+    const newId = result.rows[0].id;
+    console.log(`New product id = ${newId}`);
+    res.status(200).json({ lastId: newId });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred" });
