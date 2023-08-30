1a4e4366a264f382d289216a4d8094523f93d267
Maksim Lukianenko <maxbuzz8694@gmail.com>
2023-08-30 01:05:09 +0100

    feat(routes.js): add new route to create orders for a specific customer
    feat(routes.js): add new route to insert product availability information

--- week-2/mandatory/3-api/routes.js
+++ week-2/mandatory/3-api/routes.js
@@ -101,4 +101,74 @@ router.post("/products", async (req, res) => {
   }
 });
 
+router.post("/products/:cid/orders", async (req, res) => {
+  const custId = req.body.cust_id;
+  const orderDate = req.body.order_date;
+  const orderReference = req.body.order_reference;
+
+  try {
+    const result = await db.query(
+      "SELECT * FROM customers \
+    WHERE id = $1 ",
+      [custId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(400).json({ error: "Customer doesn't exist" });
+    }
+
+    const response = await db.query(
+      "SELECT * FROM orders \
+    WHERE order_reference = $1",
+      [orderReference]
+    );
+
+    if (response.rows.length > 0) {
+      return res.status(400).json({ error: "Order already exists" });
+    }
+
+    await db.query(
+      "INSERT INTO orders (order_date, order_reference, customer_id) \
+    VALUES ($1, $2, $3)",
+      [orderDate, orderReference, custId]
+    );
+    res.status(201).json({ message: "Order successfully created" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.post("/availability", async (req, res) => {
+  const prodId = req.body.product_id;
+  const suppId = req.body.supplier_id;
+  const unitPrice = req.body.unit_price;
+
+  if (prodId < 1 || prodId > 9) {
+    return res.status(400).json({ error: "Out of valid product range" });
+  }
+  if (suppId < 1 || suppId > 4) {
+    return res.status(400).json({ error: "Out of valid supplier range" });
+  }
+
+  try {
+    await db.query(
+      "INSERT INTO product_availability (prod_id, supp_id, unit_price) \
+    VALUES ($1, $2, $3)",
+      [prodId, suppId, unitPrice]
+    );
+
+    const insertedProduct = await db.query(
+      "SELECT * FROM product_availability WHERE prod_id = $1 AND supp_id = $2",
+      [prodId, suppId]
+    );
+
+    console.log(`New product is = ${insertedProduct.rows[0]}`);
+    res.status(200).json({ newAvailability: insertedProduct.rows[0] });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
 module.exports = router;
